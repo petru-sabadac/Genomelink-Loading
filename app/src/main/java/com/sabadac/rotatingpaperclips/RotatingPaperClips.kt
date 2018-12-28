dc0c5efb24c5847bd1470a71a8d73232cc685ba0
Petru Sabadac <petru.sabadac@outlook.com>
2018-12-28 02:59:19 +0200

    Added animation

--- app/src/main/java/com/sabadac/rotatingpaperclips/RotatingPaperClips.kt
+++ app/src/main/java/com/sabadac/rotatingpaperclips/RotatingPaperClips.kt
@@ -1,5 +1,6 @@
 package com.sabadac.rotatingpaperclips
 
+import android.animation.*
 import android.content.Context
 import android.graphics.*
 import android.support.v4.content.ContextCompat
@@ -14,18 +15,64 @@ class RotatingPaperClips @JvmOverloads constructor(
 ) :
     View(context, attrs, defStyleAttr) {
 
+    private val angleProperty = "angle"
+    private val animationDuration = 2000L
     private val canvasSide = dpToPx(400, context)
     private val middleCircleRadius = 40
-    private val widthOfShapes = 12
+    private val widthOfShapes = 14
     private val cornerRadius = 80
     private val maxWidth = 360
     private val maxHeight = 68
-
+    private var angle = 60f
     private val buttonRectF = RectF()
     private val bitmap = Bitmap.createBitmap(canvasSide.toInt(), canvasSide.toInt(), Bitmap.Config.ARGB_8888)
     private val bitmapCanvas = Canvas(bitmap)
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    init {
+        val first = firstAnimation()
+        val second = secondAnimation()
+
+        val animatorSet = AnimatorSet()
+        animatorSet.playSequentially(first, second)
+        animatorSet.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super.onAnimationEnd(animation)
+                animatorSet.start()
+            }
+        })
+
+        animatorSet.start()
+    }
+
+    private fun firstAnimation(): ValueAnimator {
+        val anglePropertyHolder = PropertyValuesHolder.ofFloat(angleProperty, 60f, 240f)
+        val valueAnimator = ValueAnimator()
+        valueAnimator.setValues(anglePropertyHolder)
+        valueAnimator.duration = animationDuration
+        valueAnimator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
+            override fun onAnimationUpdate(animation: ValueAnimator?) {
+                angle = animation?.getAnimatedValue(angleProperty) as Float
+                invalidate()
+            }
+        })
+        return valueAnimator
+    }
+
+    private fun secondAnimation(): ValueAnimator {
+        val anglePropertyHolder = PropertyValuesHolder.ofFloat(angleProperty, 240f, 420f)
+        val valueAnimator = ValueAnimator()
+        valueAnimator.setValues(anglePropertyHolder)
+        valueAnimator.duration = animationDuration
+        valueAnimator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
+            override fun onAnimationUpdate(animation: ValueAnimator?) {
+                angle = animation?.getAnimatedValue(angleProperty) as Float
+                invalidate()
+            }
+        })
+        return valueAnimator
+    }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
@@ -40,13 +87,16 @@ class RotatingPaperClips @JvmOverloads constructor(
         buttonRectF.top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
         buttonRectF.right = (bitmap.width + dpToPx(maxWidth, context)) / 2f
         buttonRectF.bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        bitmapCanvas.rotate(angle, bitmap.width / 2f, bitmap.height / 2f)
         bitmapCanvas.drawRoundRect(
             buttonRectF,
             dpToPx(cornerRadius / 2, context),
             dpToPx(cornerRadius / 2, context),
             paint
         )
+        bitmapCanvas.restore()
 
+        bitmapCanvas.save()
         paint.color = ContextCompat.getColor(context, R.color.bottomSimpleRounderRect)
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = dpToPx(widthOfShapes, context)
@@ -54,6 +104,7 @@ class RotatingPaperClips @JvmOverloads constructor(
         buttonRectF.top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
         buttonRectF.right = (bitmap.width + dpToPx(maxHeight, context)) / 2f
         buttonRectF.bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        bitmapCanvas.rotate(angle, bitmap.width / 2f, bitmap.height / 2f)
         bitmapCanvas.drawRoundRect(
             buttonRectF,
             dpToPx(cornerRadius / 2, context),
@@ -70,7 +121,7 @@ class RotatingPaperClips @JvmOverloads constructor(
         buttonRectF.right = (bitmap.width + dpToPx(maxWidth, context)) / 2f
         buttonRectF.bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
 
-        bitmapCanvas.rotate(90f, bitmap.width / 2f, bitmap.height / 2f)
+        bitmapCanvas.rotate(-1 * angle, bitmap.width / 2f, bitmap.height / 2f)
         bitmapCanvas.drawRoundRect(
             buttonRectF,
             dpToPx(cornerRadius / 2, context),
@@ -87,7 +138,7 @@ class RotatingPaperClips @JvmOverloads constructor(
         buttonRectF.top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
         buttonRectF.right = (bitmap.width + dpToPx(maxHeight, context)) / 2f
         buttonRectF.bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
-        bitmapCanvas.rotate(90f, bitmap.width / 2f, bitmap.height / 2f)
+        bitmapCanvas.rotate(-1 * angle, bitmap.width / 2f, bitmap.height / 2f)
         bitmapCanvas.drawRoundRect(
             buttonRectF,
             dpToPx(cornerRadius / 2, context),
