1fee3d21a08c58b71f295392615175d35861d197
Petru <Sabadac>
2019-01-18 22:44:32 +0200

    Updated interpolator, added method to stop animation, updated animation width and height and optimized code for dpToPx conversion

--- app/src/main/java/com/sabadac/rotatingpaperclips/GenomelinkLoading.kt
+++ app/src/main/java/com/sabadac/rotatingpaperclips/GenomelinkLoading.kt
@@ -2,8 +2,10 @@ package com.sabadac.rotatingpaperclips
 
 import android.animation.*
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.*
 import android.support.v4.content.ContextCompat
+import android.support.v4.view.animation.PathInterpolatorCompat
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
@@ -17,13 +19,13 @@ class GenomelinkLoading @JvmOverloads constructor(
 
     private val angleProperty = "angle"
     private val animationDuration = 1900L
-    private val canvasSide = dpToPx(400, context)
-    private val middleCircleRadius = 40
-    private val widthOfShapes = 14
-    private val cornerRadius = 80
-    private val maxWidth = 360
-    private val maxHeight = 68
-    private var angle = 60f
+    private val canvasSide = dpToPx(200)
+    private val middleCircleRadius = 20
+    private val widthOfShapes = 7
+    private val cornerRadius = 40
+    private val maxWidth = 180
+    private val maxHeight = 34
+    private var angle = 30f
     private val bitmap = Bitmap.createBitmap(canvasSide.toInt(), canvasSide.toInt(), Bitmap.Config.ARGB_8888)
     private val bitmapCanvas = Canvas(bitmap)
     private val shapes = Array(7) { RectF() }
@@ -31,18 +33,25 @@ class GenomelinkLoading @JvmOverloads constructor(
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val bitmapPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val animatorSet = AnimatorSet()
 
     init {
-        paint.strokeWidth = dpToPx(widthOfShapes, context)
+        paint.strokeWidth = dpToPx(widthOfShapes)
         initShapesAndColors()
         loadAnimations()
+    }
+
+    override fun clearAnimation() {
+        animatorSet.removeAllListeners()
+        animatorSet.cancel()
+        super.clearAnimation()
     }
 
     private fun loadAnimations() {
         val first = firstAnimation()
         val second = secondAnimation()
 
-        val animatorSet = AnimatorSet()
+        animatorSet.interpolator = PathInterpolatorCompat.create(0.42f, 0.0f, 0.58f, 1.0f)
         animatorSet.playSequentially(first, second)
         animatorSet.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator?) {
@@ -56,46 +65,46 @@ class GenomelinkLoading @JvmOverloads constructor(
 
     private fun initShapesAndColors() {
         colors[0] = ContextCompat.getColor(context, R.color.topSimpleRounderRect)
-        shapes[0].left = (bitmap.width - dpToPx(maxWidth, context)) / 2f
-        shapes[0].top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
-        shapes[0].right = (bitmap.width + dpToPx(maxWidth, context)) / 2f
-        shapes[0].bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        shapes[0].left = (bitmap.width - dpToPx(maxWidth)) / 2f
+        shapes[0].top = (bitmap.height - dpToPx(maxHeight)) / 2f
+        shapes[0].right = (bitmap.width + dpToPx(maxWidth)) / 2f
+        shapes[0].bottom = (bitmap.height + dpToPx(maxHeight)) / 2f
 
         colors[1] = ContextCompat.getColor(context, R.color.bottomSimpleRounderRect)
-        shapes[1].left = (bitmap.width - dpToPx(maxWidth, context)) / 2f
-        shapes[1].top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
-        shapes[1].right = (bitmap.width + dpToPx(middleCircleRadius, context)) / 2f
-        shapes[1].bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        shapes[1].left = (bitmap.width - dpToPx(maxWidth)) / 2f
+        shapes[1].top = (bitmap.height - dpToPx(maxHeight)) / 2f
+        shapes[1].right = (bitmap.width + dpToPx(middleCircleRadius)) / 2f
+        shapes[1].bottom = (bitmap.height + dpToPx(maxHeight)) / 2f
 
         colors[2] = ContextCompat.getColor(context, R.color.topComplexRounderRect)
-        shapes[2].left = (bitmap.width - dpToPx(maxWidth, context)) / 2f
-        shapes[2].top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
-        shapes[2].right = (bitmap.width + dpToPx(maxWidth, context)) / 2f
-        shapes[2].bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        shapes[2].left = (bitmap.width - dpToPx(maxWidth)) / 2f
+        shapes[2].top = (bitmap.height - dpToPx(maxHeight)) / 2f
+        shapes[2].right = (bitmap.width + dpToPx(maxWidth)) / 2f
+        shapes[2].bottom = (bitmap.height + dpToPx(maxHeight)) / 2f
 
         colors[3] = ContextCompat.getColor(context, R.color.bottomComplexRounderRect)
-        shapes[3].left = (bitmap.width - dpToPx(maxWidth, context)) / 2f
-        shapes[3].top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
-        shapes[3].right = (bitmap.width + dpToPx(middleCircleRadius, context)) / 2f
-        shapes[3].bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        shapes[3].left = (bitmap.width - dpToPx(maxWidth)) / 2f
+        shapes[3].top = (bitmap.height - dpToPx(maxHeight)) / 2f
+        shapes[3].right = (bitmap.width + dpToPx(middleCircleRadius)) / 2f
+        shapes[3].bottom = (bitmap.height + dpToPx(maxHeight)) / 2f
 
         colors[4] = ContextCompat.getColor(context, R.color.bottomComplexRounderRect)
-        shapes[4].left = (bitmap.width - dpToPx(maxWidth, context)) / 2f
-        shapes[4].top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
-        shapes[4].right = (bitmap.width - dpToPx(maxWidth, context)) / 2f + dpToPx(maxHeight, context)
-        shapes[4].bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        shapes[4].left = (bitmap.width - dpToPx(maxWidth)) / 2f
+        shapes[4].top = (bitmap.height - dpToPx(maxHeight)) / 2f
+        shapes[4].right = (bitmap.width - dpToPx(maxWidth)) / 2f + dpToPx(maxHeight)
+        shapes[4].bottom = (bitmap.height + dpToPx(maxHeight)) / 2f
 
         colors[5] = ContextCompat.getColor(context, R.color.topComplexRounderRect)
-        shapes[5].left = (bitmap.width + dpToPx(maxWidth, context)) / 2f - dpToPx(maxHeight, context)
-        shapes[5].top = (bitmap.height - dpToPx(maxHeight, context)) / 2f
-        shapes[5].right = (bitmap.width + dpToPx(maxWidth, context)) / 2f
-        shapes[5].bottom = (bitmap.height + dpToPx(maxHeight, context)) / 2f
+        shapes[5].left = (bitmap.width + dpToPx(maxWidth)) / 2f - dpToPx(maxHeight)
+        shapes[5].top = (bitmap.height - dpToPx(maxHeight)) / 2f
+        shapes[5].right = (bitmap.width + dpToPx(maxWidth)) / 2f
+        shapes[5].bottom = (bitmap.height + dpToPx(maxHeight)) / 2f
 
         colors[6] = ContextCompat.getColor(context, R.color.middleCircle)
-        shapes[6].left = (bitmap.width - dpToPx(cornerRadius, context)) / 2f
-        shapes[6].top = (bitmap.height - dpToPx(cornerRadius, context)) / 2f
-        shapes[6].right = (bitmap.width + dpToPx(cornerRadius, context)) / 2f
-        shapes[6].bottom = (bitmap.height + dpToPx(cornerRadius, context)) / 2f
+        shapes[6].left = (bitmap.width - dpToPx(cornerRadius)) / 2f
+        shapes[6].top = (bitmap.height - dpToPx(cornerRadius)) / 2f
+        shapes[6].right = (bitmap.width + dpToPx(cornerRadius)) / 2f
+        shapes[6].bottom = (bitmap.height + dpToPx(cornerRadius)) / 2f
     }
 
     private fun firstAnimation(): ValueAnimator {
@@ -103,12 +112,10 @@ class GenomelinkLoading @JvmOverloads constructor(
         val valueAnimator = ValueAnimator()
         valueAnimator.setValues(anglePropertyHolder)
         valueAnimator.duration = animationDuration
-        valueAnimator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
-            override fun onAnimationUpdate(animation: ValueAnimator?) {
-                angle = animation?.getAnimatedValue(angleProperty) as Float
-                invalidate()
-            }
-        })
+        valueAnimator.addUpdateListener { animation ->
+            angle = animation.getAnimatedValue(angleProperty) as Float
+            invalidate()
+        }
         return valueAnimator
     }
 
@@ -117,12 +124,10 @@ class GenomelinkLoading @JvmOverloads constructor(
         val valueAnimator = ValueAnimator()
         valueAnimator.setValues(anglePropertyHolder)
         valueAnimator.duration = animationDuration
-        valueAnimator.addUpdateListener(object : ValueAnimator.AnimatorUpdateListener {
-            override fun onAnimationUpdate(animation: ValueAnimator?) {
-                angle = animation?.getAnimatedValue(angleProperty) as Float
-                invalidate()
-            }
-        })
+        valueAnimator.addUpdateListener { animation ->
+            angle = animation.getAnimatedValue(angleProperty) as Float
+            invalidate()
+        }
         return valueAnimator
     }
 
@@ -144,8 +149,8 @@ class GenomelinkLoading @JvmOverloads constructor(
             paint.color = colors[index]
             bitmapCanvas.drawRoundRect(
                 shape,
-                dpToPx(cornerRadius / 2, context),
-                dpToPx(cornerRadius / 2, context),
+                dpToPx(cornerRadius / 2),
+                dpToPx(cornerRadius / 2),
                 paint
             )
             bitmapCanvas.restore()
@@ -154,7 +159,7 @@ class GenomelinkLoading @JvmOverloads constructor(
         canvas?.drawBitmap(bitmap, (width - bitmap.width) / 2f, (height - bitmap.height) / 2f, bitmapPaint)
     }
 
-    private fun dpToPx(dp: Int, context: Context): Float =
-        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), context.resources.displayMetrics)
+    private fun dpToPx(dp: Int): Float =
+        TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp.toFloat(), Resources.getSystem().displayMetrics)
 
 }
